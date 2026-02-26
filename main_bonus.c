58d40e227ba3c96f307cce6d7880bfaa17e3aefb
Tom Lebon <tlebon@f6r9s16.clusters.42paris.fr>
2026-02-26 21:11:14 +0100

    ft_list_sort terminee a bien tester peut etre

--- main_bonus.c
+++ main_bonus.c
@@ -32,32 +32,40 @@ int main(int ac, char **av)
     ft_list_push_front(&begin_list, data2);
     printf("\nNew link data address: %p\n", begin_list->data);
     printf("New link data value: %s\n", (char *)begin_list->data);
-    ft_list_push_front(&begin_list, data3);
-    printf("\nNew link data address: %p\n", begin_list->data);
-    printf("New link data value: %s\n", (char *)begin_list->data);
-    ft_list_push_front(&begin_list, data2);
-    printf("\nNew link data address: %p\n", begin_list->data);
-    printf("New link data value: %s\n", (char *)begin_list->data);
-    ft_list_push_front(&begin_list, data1);
-    printf("\nNew link data address: %p\n", begin_list->data);
-    printf("New link data value: %s\n", (char *)begin_list->data);
+    // ft_list_push_front(&begin_list, data3);
+    // printf("\nNew link data address: %p\n", begin_list->data);
+    // printf("New link data value: %s\n", (char *)begin_list->data);
+    // ft_list_push_front(&begin_list, data1);
+    // printf("\nNew link data address: %p\n", begin_list->data);
+    // printf("New link data value: %s\n", (char *)begin_list->data);
+    // ft_list_push_front(&begin_list, data3);
+    // printf("\nNew link data address: %p\n", begin_list->data);
+    // printf("New link data value: %s\n", (char *)begin_list->data);
 
     int lSize = ft_list_size(begin_list);
 
     printf("List size = %d\n", lSize);
-
-    ft_list_sort(&begin_list, strcmp);
-
+    
     int i = 0;
+    t_list *curs = begin_list;
+    printf("BEFORE\n");
     while (begin_list)
     {
         printf("Node %d data: %s\n", i++, (char *)begin_list->data);
         begin_list = begin_list->next;
     }
 
-    printf("cmp data1 data2: %d\n", strcmp("data1", "data2"));
-    printf("cmp data2 data1: %d\n", strcmp("data2", "data1"));
+    begin_list = curs;
+    ft_list_sort(&begin_list, strcmp);
+    printf("\n\n");
 
+    i = 0;
+    printf("AFTER\n");
+    while (begin_list)
+    {
+        printf("Node %d data: %s\n", i++, (char *)begin_list->data);
+        begin_list = begin_list->next;
+    }
 
     return (0);
 }
--- src_bonus/ft_list_sort_bonus.s
+++ src_bonus/ft_list_sort_bonus.s
@@ -12,9 +12,6 @@ section .text
 global ft_list_sort
 extern ft_list_push_front
 
-insert_node:
-
-
 
 ; void ft_list_sort(t_list **begin_list, int (*cmp)())
 ft_list_sort:
@@ -26,27 +23,92 @@ ft_list_sort:
 	mov [rbp - 8], rdi				; **begin_list
 	mov [rbp - 16], rsi 			; *cmp
 
-
 	push rbx
 	push r12
+	push r13
+	push r14
+	push r15
+	sub rsp, 8
 	mov rdi, [rdi]
+
+	cmp rdi, 0
+	je .emptylist
+
 	mov rbx, [rdi + s_list.next]	; cursor sur la liste non triee
-	mov [rdi + s_list.next], 0
+	mov qword [rdi + s_list.next], 0
 	mov r12, rdi
 
 	.while: 
 		cmp rbx, 0					; head == NULL
 		je .return
 
-		; utiliser r12 comme pointeur vers sorted + le noeud de la liste courrante a trier
-		call insert_node
+		; rbx = noeud a inserer
+		; r12 = tete de liste sorted
+		; r13 = cursor sur sorted			| rcx
+		; r14 = save rbx->next				| r9
+		; r15 = save noeud d'avant			| r8
 
-		mov rbx, [rbx + s_list.next]	; rbp24 contient l'adresse du premier node
+
+
+		mov r13, r12	
+		mov r14, [rbx + s_list.next]
+		mov qword [rbx + s_list.next], 0
+			.innerwhile:			; Boucle de comparaison data: current vs sorted data
+				cmp qword r13, 0
+				je .appendend
+
+				mov rdi, [rbx + s_list.data]	; current->data
+				mov rsi, [r13 + s_list.data]	; sorted->data
+				call [rbp - 16]
+
+				cmp eax, 0
+				jle .appendbefore
+
+				mov r15, r13
+				mov r13, [r13 + s_list.next] 
+				jmp .innerwhile
+			.appendend:
+				mov [r15 + s_list.next], rbx
+				jmp .innerend
+			.appendbefore:
+
+				cmp r13, r12
+				je .appendbegin
+
+				; A -> C | on veut inserer B
+				; set A.next = B
+				; set B.next = C
+				mov [r15 + s_list.next], rbx
+				mov [rbx + s_list.next], r13
+
+				jmp .innerend
+			
+			.appendbegin:
+				mov [rbx + s_list.next], r13
+				mov r12, rbx
+				jmp .innerend
+
+			.innerend:
+
+		mov rbx, r14
 		jmp .while
 
 	.return:
 		mov rdi, [rbp - 8]
 		mov [rdi], r12
+		pop r15
+		pop r14
+		pop r13
+		pop r12
+		pop rbx
+		mov rsp, rbp
+		pop rbp
+		ret
+
+	.emptylist:
+		pop r15
+		pop r14
+		pop r13
 		pop r12
 		pop rbx
 		mov rsp, rbp
